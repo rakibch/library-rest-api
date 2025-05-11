4b4dc2f57754996ba0f87d0f79290d8fae423e1c
rakibch <rakib9204@gmail.com>
2025-05-11 17:57:44 +0600

    renamed search controller and crated routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\API\BookshelfController;
+use App\Http\Controllers\API\BookController;
+use App\Http\Controllers\API\ChapterController;
+use App\Http\Controllers\API\PageController;
+use App\Http\Controllers\SearchController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::apiResource('bookshelves', BookshelfController::class);
-//     Route::apiResource('books', BookController::class);
-//     Route::apiResource('chapters', ChapterController::class);
-//     Route::apiResource('pages', PageController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('bookshelves', BookshelfController::class);
+    Route::apiResource('books', BookController::class);
+    Route::apiResource('chapters', ChapterController::class);
+    Route::apiResource('pages', PageController::class);
 
-//     Route::get('search-books', [SearchController::class, 'search']);
-//     Route::get('chapters/{id}/full-content', [ChapterController::class, 'fullContent']);
+    Route::get('search-books', [SearchController::class, 'search']);
+    Route::get('chapters/{id}/full-content', [ChapterController::class, 'fullContent']);
 
-//     Route::post('/logout', [AuthController::class, 'logout']);
-// });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
