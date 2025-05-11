effe9d8cb4956fdf7e0efa955c8713c1d0ce6b09
rakibch <rakib9204@gmail.com>
2025-05-11 18:00:22 +0600

    added all the code for bookshelf

--- app/Http/Controllers/API/BookshelfController.php
+++ app/Http/Controllers/API/BookshelfController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Bookshelf;
 
 class BookshelfController extends Controller
 {
@@ -12,7 +13,7 @@ class BookshelfController extends Controller
      */
     public function index()
     {
-        //
+        return Bookshelf::all();
     }
 
     /**
@@ -20,30 +21,37 @@ class BookshelfController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string',
+            'location' => 'required|string',
+        ]);
+
+        return Bookshelf::create($request->only('name', 'location'));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Bookshelf $bookshelf)
     {
-        //
+        return $bookshelf;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Bookshelf $bookshelf)
     {
-        //
+        $bookshelf->update($request->only('name', 'location'));
+        return $bookshelf;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Bookshelf $bookshelf)
     {
-        //
+        $bookshelf->delete();
+        return response()->noContent();
     }
 }
