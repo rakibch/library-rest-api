8ac0d3947aead59449aabeeb46815a43a9b343d3
rakibch <rakib9204@gmail.com>
2025-05-11 18:08:47 +0600

    added chapter controller with fullContent function

--- app/Http/Controllers/API/ChapterController.php
+++ app/Http/Controllers/API/ChapterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Chapter;
+use App\Models\Page;
 
 class ChapterController extends Controller
 {
@@ -12,7 +14,7 @@ class ChapterController extends Controller
      */
     public function index()
     {
-        //
+        return Chapter::with('book')->get();
     }
 
     /**
@@ -20,30 +22,50 @@ class ChapterController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         $request->validate([
+            'book_id' => 'required|exists:books,id',
+            'title' => 'required|string',
+            'chapter_number' => 'required|integer',
+        ]);
+
+        return Chapter::create($request->all());
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Chapter $chapter)
     {
-        //
+        return $chapter->load('book');
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Chapter $chapter)
     {
-        //
+        $chapter->update($request->all());
+        return $chapter;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Chapter $chapter)
     {
-        //
+        $chapter->delete();
+        return response()->noContent();
+    }
+
+    public function fullContent($id)
+    {
+        $chapter = Chapter::with('pages')->findOrFail($id);
+
+        $fullContent = $chapter->pages->sortBy('page_number')->pluck('content')->implode("\n\n");
+
+        return response()->json([
+            'chapter_title' => $chapter->title,
+            'full_content' => $fullContent,
+        ]);
     }
 }
