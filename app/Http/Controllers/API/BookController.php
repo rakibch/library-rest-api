8c6f8d6322998464a0ae9f8fa32ad823598ea7d7
rakibch <rakib9204@gmail.com>
2025-05-11 18:03:09 +0600

    add functionalities for book controller

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
@@ -12,7 +13,7 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
+        return Book::with('bookshelf')->get();
     }
 
     /**
@@ -20,30 +21,39 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'bookshelf_id' => 'required|exists:bookshelves,id',
+            'title' => 'required|string',
+            'author' => 'required|string',
+            'published_year' => 'required|digits:4',
+        ]);
+
+        return Book::create($request->all());
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return $book->load('bookshelf');
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Book $book)
     {
-        //
+        $book->update($request->all());
+        return $book;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Book $book)
     {
-        //
+        $book->delete();
+        return response()->noContent();
     }
 }
